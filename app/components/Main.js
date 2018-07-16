a4eb13c24286a0dde7f6ab1b9bc3eae453db686e
Mateusz Cygal <mateusz.cygal@d3r.com>
2018-07-17 00:22:06 +0100

    Add files via upload

---
+++ app/components/Main.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, ScrollView, TouchableOpacity, KeyboardAvoidingView } from 'react-native';
+
+import Note from './Note';
+
+export default class Main extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            noteArray: [],
+            noteText: ''
+        }
+    }
+
+  render() {
+      let notes = this.state.noteArray.map((val, key) => {
+          return <Note key={key} keyval={key} val={val} deleteMethod={  () => this.deleteNote(key) } />
+      });
+    return (
+    <KeyboardAvoidingView style={styles.container}>
+        <View style={styles.header}>
+            <Text style={styles.headerText}>Notatki</Text>
+        </View>
+        <ScrollView style={styles.scrollContainer} >
+            {notes}
+        </ScrollView>
+        <View style={styles.footer}>
+            <TextInput style={styles.textInput}
+            onChangeText={(noteText) => this.setState({noteText})}
+            value={this.state.noteText}
+            placeholder='>note'
+            placeholderTextColor='white'
+            underlineColorAndroid='transparent'>
+            </TextInput>
+        </View>
+        <TouchableOpacity style={styles.addButton} onPress={this.addNote.bind(this)}>
+            <Text style={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
+    </KeyboardAvoidingView>
+    );
+  }
+  addNote() {
+      if (this.state.noteText) {
+          var d = new Date();
+          this.state.noteArray.push({
+            'date': d.getFullYear() + "/" +
+            (d.getMonth() + 1) + "/" +
+            d.getDate(),
+            'note': this.state.noteText
+          });
+          this.setState({ noteArray: this.state.noteArray });
+          this.setState({ noteText: '' })
+      }
+  }
+  deleteNote(key) {
+      this.state.noteArray.splice(key, 1);
+      this.setState({ noteArray: this.state.noteArray })
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+  },
+  header: {
+      backgroundColor: '#0173c7',
+      alignItems: 'center',
+      justifyContent:'center',
+      borderBottomWidth: 10,
+      borderBottomColor: '#017bd5'
+  },
+  headerText: {
+      color: 'white',
+      fontSize: 18,
+      padding: 26
+  },
+  scrollContainer: {
+      flex: 1,
+      marginBottom: 100
+  },
+  footer: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      zIndex: 10
+  },
+  textInput: {
+      alignSelf: 'stretch',
+      color: '#fff',
+      padding: 20,
+      backgroundColor: '#0173c7',
+      borderTopWidth:2,
+      borderTopColor: '#ededed'
+  },
+  addButton: {
+      position: 'absolute',
+      zIndex: 11,
+      right: 20,
+      bottom: 75,
+      backgroundColor: '#017bd5',
+      width: 50,
+      height: 50,
+      borderRadius: 35,
+      alignItems: 'center',
+      justifyContent: 'center',
+      elevation: 0
+  },
+  addButtonText: {
+      color: '#fff',
+      fontSize: 50,
+      paddingBottom: 66,
+      paddingLeft: 2
+  }
+});
---
+++ app/data/flatListData.js
@@ -0,0 +1,15 @@
+var flatListData = [
+    {
+        key: 1,
+        title: "Zrobic apke"
+    },
+    {
+        key: 2,
+        title: "Zjesc sniadanie"
+    },
+    {
+        key: 3,
+        title: "Council"
+    }
+];
+module.exports = flatListData;
